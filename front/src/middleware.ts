6913cefc2319a70e6162f4d9f103ac9186450ecd
Kanae-Nishina <karubona.ra247@gmail.com>
2024-04-15 12:48:27 +0900

    フロント国際化の初期設定

---
+++ front/src/middleware.ts
@@ -0,0 +1,11 @@
+import createMiddleware from "next-intl/middleware";
+import { locales } from "./lib";
+
+export default createMiddleware({
+  defaultLocale: "ja",
+  locales,
+});
+
+export const config = {
+  matcher: ["/", "/(ja|en)/:path*"],
+};
