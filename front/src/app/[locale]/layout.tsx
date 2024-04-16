4a5008988605714972293e621a35d16ecd14e59b
Kanae-Nishina <karubona.ra247@gmail.com>
2024-04-16 15:02:41 +0900

    ログインを促すモーダル作成

--- front/src/app/[locale]/layout.tsx
+++ front/src/app/[locale]/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
-import * as Layout from "@/components/layouts";
 import { NextIntlClientProvider, useMessages } from "next-intl";
+import AppProvider from "@/providers/index";
+import { MainLayout } from "@/components/layouts";
 
 export const metadata: Metadata = {
   title: "AStoryer - あすとりや -",
@@ -17,17 +18,13 @@ export default function RootLayout({
   const messages = useMessages();
   return (
     <html lang="ja">
-      <NextIntlClientProvider locale={locale} messages={messages}>
-        <body className="w-screen min-h-screen flex flex-col bg-slate-200 relative">
-          <div className="w-full bg-white shadow-sm">
-            <Layout.Headers />
-          </div>
-          <main className="w-full flex-1">{children}</main>
-          <div className="w-full bg-slate-500">
-            <Layout.Footers />
-          </div>
-        </body>
-      </NextIntlClientProvider>
+      <AppProvider>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <body className="w-screen min-h-screen flex flex-col bg-slate-200 relative">
+            <MainLayout>{children}</MainLayout>
+          </body>
+        </NextIntlClientProvider>
+      </AppProvider>
     </html>
   );
 }
---
+++ front/src/recoilState/modalState.ts
@@ -0,0 +1,12 @@
+"use client";
+import { atom } from "recoil";
+
+export const modalOpenState = atom({
+  key: "modalOpenState",
+  default: false,
+});
+
+export const modalTitleState = atom({
+  key: "modalTitleState",
+  default: "",
+});
