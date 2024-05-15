1a753c7213e1af5ef4a24eed3c0cdb01070d12d8
Kanae-Nishina <karubona.ra247@gmail.com>
2024-05-15 14:23:51 +0900

    APIからシナリオ名取得

--- back/app/controllers/api/v1/tags_controller.rb
+++ back/app/controllers/api/v1/tags_controller.rb
@@ -1,4 +1,5 @@
-class Api::V1::TagsController < ApplicationController
+class Api::V1::TagsController < Api::V1::BasesController
+  skip_before_action :authenticate_api_v1_user!, only: %i[index]
   def index
     tags = Tag.all.map(&:name)
     render json: tags, status: :ok
