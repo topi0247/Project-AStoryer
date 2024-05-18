51da4b8194af325357a3763fefc77345739542da
Kanae-Nishina <karubona.ra247@gmail.com>
2024-05-18 14:39:29 +0900

    詳細検索でAND検索、OR検索を含めた複雑な検索を実装

--- back/app/controllers/api/v1/illusts_controller.rb
+++ back/app/controllers/api/v1/illusts_controller.rb
@@ -2,21 +2,26 @@ class Api::V1::IllustsController < Api::V1::BasesController
   skip_before_action :authenticate_api_v1_user!, only: %i[index]
 
   def index
+    post_search = PostSearch.new(search_params)
+    search_results = []
     if search_params[:search_word].present?
-      post_search = PostSearch.new(search_params)
       search_results = post_search.search_word
-      if search_results.present?
-        posts_json = search_results.map do |post|
-          content = nil
-          if post.illust?
-            content = url_for(post.postable.image)
-          end
-          post.as_custom_index_json(content)
-        end
-      else
-      end
-      render json: {illusts: posts_json, count: search_results.count}, status: :ok
+    else
+      search_results = post_search.search_detail
     end
+
+    posts_json = []
+    if search_results.present?
+      posts_json = search_results.map do |post|
+        content = nil
+        if post.illust?
+          content = url_for(post.postable.image)
+        end
+        post.as_custom_index_json(content)
+      end
+    end
+
+    render json: { illusts: posts_json, count: search_results.count }, status: :ok
   end
 
   private
--- back/app/lib/post_search.rb
+++ back/app/lib/post_search.rb
@@ -22,4 +22,38 @@ class PostSearch
       posts
     end
   end
+
+  def search_detail
+    post_title = @params[:post_title]
+    game_system = @params[:game_system].present? ? GameSystem.find_by_name(@params[:game_system]) : nil
+    synalio_name = @params[:synalio_name]
+    tags = @params[:tags]
+    tag_list = tags.split(',') if tags.present?
+    user_name = @params[:user_name]
+    search_type = @params[:search_type]
+
+    posts = []
+    # ゲームシステムで検索
+    if game_system.present?
+      game_systems = PostGameSystem.where(game_system_id: game_system.id)
+      posts = Post.where(id: game_systems.map(&:post_id))
+              .where(publish_state: 'all_publish').order(published_at: :desc)
+    end
+
+    # AND検索
+    if search_type == 'AND'
+      return posts if posts.blank?
+      # タイトル、タグ、シナリオ名、ユーザー名で検索
+      posts = posts.joins(:user, :tags, :synalios)
+              .where(publish_state: 'all_publish')
+              .where('posts.title LIKE ? AND tags.name LIKE ? AND synalios.name LIKE ? AND users.name LIKE ?', "%#{post_title}%", "%#{tag_list}%", "%#{synalio_name}%", "%#{user_name}%")
+      posts
+    else
+      # OR検索
+      posts = Post.joins(:user, :tags, :synalios)
+              .where(publish_state: 'all_publish')
+              .where('posts.title LIKE ? OR tags.name LIKE ? OR synalios.name LIKE ? OR users.name LIKE ?', "%#{post_title}%", "%#{tag_list}%", "%#{synalio_name}%", "%#{user_name}%").merge(posts)
+      posts
+    end
+  end
 end
