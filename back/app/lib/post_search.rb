1be84176a02e10464421d154141e9ec411cef85d
Kanae-Nishina <karubona.ra247@gmail.com>
2024-05-18 14:15:07 +0900

    検索ワードでゲームシステム以外の投稿を取得

--- back/app/lib/post_search.rb
+++ back/app/lib/post_search.rb
@@ -10,18 +10,16 @@ class PostSearch
     game_system = GameSystem.find_by_name(search_word)
     if game_system.present?
       game_systems = PostGameSystem.where(game_system_id: game_system.id)
-      posts = Post.includes(:user).where(id: game_systems.map(&:post_id)).where(publish_state: 'all_publish').order(published_at: :desc)
+      posts = Post.includes(:user)
+              .where(id: game_systems.map(&:post_id))
+              .where(publish_state: 'all_publish').order(published_at: :desc)
       posts
     else
-      posts = Post.includes(:user, :tags, :synalios).where(publish_state: 'all_publish')
-      posts = posts.where('title LIKE ?', "%#{search_word}%").or(posts.tags.where('name LIKE ?', "%#{search_word}%")).or(posts.synalios.where('name LIKE ?', "%#{search_word}%").or(posts.user.where('name LIKE ?', "%#{search_word}%"))).order(published_at: :desc)
-      posts.map do |post|
-        content = nil
-        if post.illust?
-          content = url_for(post.postable.image)
-        end
-        posts.as_custom_index_json(content)
-      end
+      posts = Post.joins(:user, :tags, :synalios)
+            .where(publish_state: 'all_publish')
+            .where('posts.title LIKE ? OR tags.name LIKE ? OR synalios.name LIKE ? OR users.name LIKE ?', "%#{search_word}%", "%#{search_word}%", "%#{search_word}%", "%#{search_word}%")
+            .order(published_at: :desc)
+      posts
     end
   end
 end
