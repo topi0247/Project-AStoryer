e36f7ac105d8ad81fc62a3acf733d5c6274c4d97
topi0247 <karubona.ra247@gmail.com>
2024-06-05 10:56:24 +0900

    下書き画面のローディング

--- front/src/components/features/post/caption.tsx
+++ front/src/components/features/post/caption.tsx
@@ -4,20 +4,28 @@ import { useTranslations } from "next-intl";
 export default function Caption({
   formProps,
   CAPTION_MAX_LENGTH,
+  loading = false,
 }: {
   formProps: any;
   CAPTION_MAX_LENGTH: number;
+  loading: boolean;
 }) {
   const t_PostGeneral = useTranslations("PostGeneral");
   return (
-    <Mantine.Textarea
-      name="caption"
-      label={t_PostGeneral("caption")}
-      size="sm"
-      radius="xs"
-      rows={5}
-      maxLength={CAPTION_MAX_LENGTH}
-      {...formProps}
-    />
+    <>
+      <Mantine.InputLabel>{t_PostGeneral("caption")}</Mantine.InputLabel>
+      {loading ? (
+        <Mantine.Skeleton height={108} />
+      ) : (
+        <Mantine.Textarea
+          name="caption"
+          size="sm"
+          radius="xs"
+          rows={5}
+          maxLength={CAPTION_MAX_LENGTH}
+          {...formProps}
+        />
+      )}
+    </>
   );
 }
