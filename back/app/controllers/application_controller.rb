a3267d782a61ba365d0e815b1ecd3628e878c145
topi0247 <karubona.ra247@gmail.com>
2024-06-06 17:56:02 +0900

    不要なパラメータ削除

--- back/app/controllers/application_controller.rb
+++ back/app/controllers/application_controller.rb
@@ -5,6 +5,5 @@ class ApplicationController < ActionController::API
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:password, keys: [:redirect_url])
   end
 end
