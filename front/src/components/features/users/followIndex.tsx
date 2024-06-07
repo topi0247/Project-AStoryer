e8d6dae76b4669d4df2ec6d3e8159088e835721a
topi0247 <karubona.ra247@gmail.com>
2024-06-07 12:54:07 +0900

    データフェッチ処理実装

--- front/src/components/features/users/followIndex.tsx
+++ front/src/components/features/users/followIndex.tsx
@@ -1,5 +1,11 @@
+"use client";
+
 import { IIndexFollowData, Tab } from "@/types";
 import { FollowUser } from ".";
+import { GetFromAPI } from "@/lib";
+import useSWR from "swr";
+
+const feature = (url: string) => GetFromAPI(url).then((res) => res.data);
 
 export default function FollowIndex({
   uuid,
@@ -10,23 +16,23 @@ export default function FollowIndex({
   url: string;
   tabType: Tab;
 }) {
-  const data: IIndexFollowData[] = Array.from({ length: 40 }, (_, index) => ({
-    uuid: `uuid-${index}`,
-    name: `User ${index}`,
-    avatar: `avatar-${index}`,
-  }));
+  const { data, error } = useSWR(url, feature);
 
   return (
     <section className="container my-2 m-auto">
       <div className="grid grid-cols-4 md:grid-cols-8 gap-x-2 md:gap-x-0 gap-y-8 mx-2 justify-center items-center">
-        {data ? (
-          data.map((user, i) => <FollowUser key={i} user={user} />)
-        ) : (
+        {data === undefined ? (
           <>
             {Array.from({ length: 10 }, (_, index) => (
               <FollowUser key={index} />
             ))}
           </>
+        ) : (
+          data.users.map(
+            ({ user, i }: { user: IIndexFollowData; i: number }) => (
+              <FollowUser key={i} user={user} />
+            )
+          )
         )}
       </div>
     </section>
