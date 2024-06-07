6faf1f515827af92612e9ba67388df68f0832261
topi0247 <karubona.ra247@gmail.com>
2024-06-07 12:48:45 +0900

    スケルトンローディング実装

--- front/src/components/features/users/followIndex.tsx
+++ front/src/components/features/users/followIndex.tsx
@@ -19,9 +19,15 @@ export default function FollowIndex({
   return (
     <section className="container my-2 m-auto">
       <div className="grid grid-cols-4 md:grid-cols-8 gap-x-2 md:gap-x-0 gap-y-8 mx-2 justify-center items-center">
-        {data.map((user) => (
-          <FollowUser key={user.uuid} user={user} />
-        ))}
+        {data ? (
+          data.map((user, i) => <FollowUser key={i} user={user} />)
+        ) : (
+          <>
+            {Array.from({ length: 10 }, (_, index) => (
+              <FollowUser key={index} />
+            ))}
+          </>
+        )}
       </div>
     </section>
   );
--- front/src/components/features/users/followUser.tsx
+++ front/src/components/features/users/followUser.tsx
@@ -3,21 +3,38 @@ import { RouterPath } from "@/settings";
 import { IIndexFollowData } from "@/types";
 import * as Mantine from "@mantine/core";
 
-export default function FollowUser({ user }: { user: IIndexFollowData }) {
+export default function FollowUser({
+  user = {
+    uuid: "",
+    name: "",
+    avatar: "",
+  },
+}: {
+  user?: IIndexFollowData;
+}) {
   return (
     <div>
-      <Link
-        href={RouterPath.users(user.uuid)}
-        className="flex flex-col justify-center items-center"
-      >
-        <Mantine.Avatar
-          variant="default"
-          size="xl"
-          src={user.avatar}
-          alt="avatar"
-        />
-        <p>{user.name}</p>
-      </Link>
+      {user.uuid === "" ? (
+        <>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Mantine.Skeleton width={100} height={100} radius="100%" />
+            <Mantine.Skeleton width={50} height={20} />
+          </div>
+        </>
+      ) : (
+        <Link
+          href={RouterPath.users(user.uuid)}
+          className="flex flex-col justify-center items-center"
+        >
+          <Mantine.Avatar
+            variant="default"
+            size="xl"
+            src={user.avatar}
+            alt="avatar"
+          />
+          <p>{user.name}</p>
+        </Link>
+      )}
     </div>
   );
 }
